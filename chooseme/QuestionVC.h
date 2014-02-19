56dd587edbad9626e9f4bef67230736aadf9700e
Jenny Kwan <jennylkwan@gmail.com>
2014-02-19 00:00:47 -0800

    add PostVC and add confirmation to posting flow

---
+++ chooseme/QuestionVC.h
@@ -0,0 +1,13 @@
+//
+//  QuestionVC.h
+//  chooseme
+//
+//  Created by Jenny Kwan on 2/17/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionVC : UIViewController
+
+@end
---
+++ chooseme/QuestionVC.m
@@ -0,0 +1,38 @@
+//
+//  QuestionVC.m
+//  chooseme
+//
+//  Created by Jenny Kwan on 2/17/14.
+//  Copyright (c) 2014 Jenny Kwan. All rights reserved.
+//
+
+#import "QuestionVC.h"
+
+@interface QuestionVC ()
+
+@end
+
+@implementation QuestionVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
